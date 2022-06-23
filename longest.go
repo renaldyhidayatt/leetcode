6e354165e9e9f41d9423d712aa343f9778bbddd2
renaldyhidayatt <renaldyhidayatt@gmail.com>
2022-06-23 09:56:28 +0700

    Adding longest

---
+++ longest.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func longgesCommon(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+		for strings.Index(strs[i], prefix) != 0 {
+			prefix = prefix[0 : len(prefix)-1]
+			if prefix == "" {
+				return ""
+			}
+		}
+	}
+
+	return prefix
+}
+
+func main() {
+	flower := []string{"flower", "flow", "flight"}
+	fmt.Println(longgesCommon(flower))
+}
